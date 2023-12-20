04c0f737678c1b2e9213148119846f1b1ebb38b5
Viraj-10 <virajjoshi132@gmail.com>
2023-12-21 00:09:37 +0530

    feat: added reanimated

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import React from "react";
+import {
+  Canvas,
+  Circle,
+  Group,
+  vec,
+  useImage,
+  Image,
+  Blur,
+} from "@shopify/react-native-skia";
+import { Dimensions, SafeAreaView } from "react-native";
+const App = () => {
+  const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
+  const c = vec(width / 2, height / 2);
+  const r = (width - 10) / 2;
+  const image = useImage(require("./assets/snow.jpg"));
+  if (!image) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Canvas style={{ width, height }}>
+        <Image
+          width={width}
+          height={height}
+          x={0}
+          y={0}
+          image={image}
+          fit="cover"
+        >
+          <Blur blur={0.5} />
+        </Image>
+        <Group>
+          <Circle c={c} r={5} color="red"></Circle>
+        </Group>
+      </Canvas>
+    </>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
